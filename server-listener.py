17c3619651c457cfae8b766212df82e6ddf51a58
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-03-19 11:29:38 -0400

    clean up indentation

--- server-listener.py
+++ server-listener.py
@@ -6,16 +6,16 @@ import os
 
 #dictionaries for processes
 pid = {
-	"VIDEO_STREAM": False,
-	"DIRECTION_STREAM": False,
-	"GPS_STREAM": False
+    "VIDEO_STREAM": False,
+    "DIRECTION_STREAM": False,
+    "GPS_STREAM": False
 }
 
 command = {
-	"VIDEO_STREAM": "./start_video_stream.sh",
-	"DIRECTION_STREAM": "./dofdevice.py",
-	"GPS_STREAM": "./test.py",
-	"PAN_TILT": "./pan_tilt.sh"
+    "VIDEO_STREAM": "./start_video_stream.sh",
+    "DIRECTION_STREAM": "./dofdevice.py",
+    "GPS_STREAM": "./test.py",
+    "PAN_TILT": "./pan_tilt.sh"
 }
 
 # track current pan and tilt angles
@@ -29,94 +29,93 @@ port = 9998
 
 #connect to server
 def connect():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host, port))
-	return s
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    return s
 
 #send string to socket
 def send(a_str, s):
-	s.send(a_str.encode())
+    s.send(a_str.encode())
 
 
 #recieve data from socket
 def recieve(s):
-	print("waiting")
-	command = ''
-	command = s.recv(1024).decode()
-	print (command)
-	return command
+    print("waiting")
+    command = ''
+    command = s.recv(1024).decode()
+    print (command)
+    return command
 
 
 #start process
 def start_process(input_str):
-	try:
-		process = input_str.split("|")
-		args = process[0].split(":")
-		
-		if (len(args) < 2):
-			return
+    try:
+        process = input_str.split("|")
+        args = process[0].split(":")
 
-		start_stop = args[0]
-		the_command = args[1]
-		
-		#start process
-		if (start_stop == "START"):
-			if (pid[the_command] == False):
-				process_args = [command[the_command]]
-				if (len(args) > 2):
-					for i in range (2, len(args)):
-						process_args.append(args[i])
-		
-				the_pid = os.fork()
-				#child
-				if the_pid == 0:
-					os.execl(command[the_command], *tuple(process_args))
-					assert False, 'error starting procces'
-				#parent
-				else:
-					pid[the_command] = the_pid
-					return
-		
-		#stop process
-		elif (start_stop == "STOP"):
-			if (pid[the_command] != False):
-				if (the_command == VIDEO_STREAM):
-					kill = "pkill ffmpeg"
-				else:
-					kill = "kill -9 " + str(pid[the_command])
-				
-				os.system(kill)
-				pid[the_command] = False
-	
-	except:
-		print("error in start_process")
-	
+        if (len(args) < 2):
+            return
+
+        start_stop = args[0]
+        the_command = args[1]
+
+        #start process
+        if (start_stop == "START"):
+            if (pid[the_command] == False):
+                process_args = [command[the_command]]
+                if (len(args) > 2):
+                    for i in range (2, len(args)):
+                        process_args.append(args[i])
+
+                the_pid = os.fork()
+                #child
+                if the_pid == 0:
+                    os.execl(command[the_command], *tuple(process_args))
+                    assert False, 'error starting procces'
+                #parent
+                else:
+                    pid[the_command] = the_pid
+                    return
+
+        #stop process
+        elif (start_stop == "STOP"):
+            if (pid[the_command] != False):
+                if (the_command == VIDEO_STREAM):
+                    kill = "pkill ffmpeg"
+                else:
+                    kill = "kill -9 " + str(pid[the_command])
+
+                os.system(kill)
+                pid[the_command] = False
+
+    except:
+            print("error in start_process")
+
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 isConnected = 0
 while True:
-	while isConnected == 0:
-		try:
-			s = connect()
-			isConnected = 1
-		
-		except socket.error:
-			isConnected = 0
-	while True:
-		try:
-			a_command = None
-			a_command = recieve(s)
-		
-			if a_command != None and a_command != "":
-				start_process(a_command)
-			else:
-				isConnected = 0
-				break
-		
-		except socket.error:
-			s.close()
-			isConnected = 0
-			break
+    while isConnected == 0:
+        try:
+            s = connect()
+            isConnected = 1
+
+        except socket.error:
+            isConnected = 0
+    while True:
+        try:
+            a_command = None
+            a_command = recieve(s)
+
+            if a_command != None and a_command != "":
+                start_process(a_command)
+            else:
+                isConnected = 0
+                break
+
+        except socket.error:
+            s.close()
+            isConnected = 0
+            break
 s.close()
--- socket-test.py
+++ socket-test.py
@@ -3,30 +3,30 @@
 import socket
 
 def connect():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	host = "100.91.29.141"
-	port = 9000
-	s.connect((host, port))
-	return s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "100.91.29.141"
+    port = 9000
+    s.connect((host, port))
+    return s
 
 def send(a_str, s):
-	s.send(a_str.encode())
+    s.send(a_str.encode())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-isConnected = 0
+connected = False
 while True:
-	while isConnected == 0:
-		try:
-			s = connect()
-			isConnected = 1
-		except socket.error:
-			isConnected = 0
-	while True:
-		try:
-			send("test", s)
-		
-		except socket.error:
-			s.close()
-			isConnected = 0
-			break
+    while not connected:
+        try:
+            s = connect()
+            connected = True
+        except socket.error:
+            connected = False
+    while True:
+        try:
+            send("test", s)
+
+        except socket.error:
+            s.close()
+            connected = False
+            break
 s.close()
