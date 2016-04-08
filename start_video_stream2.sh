ea6598aa2993b1632e731ba46bc1f05c3f4823cc
Aaron Wells <ajwells@email.wm.edu>
2016-04-08 15:18:15 +0000

    fix video pipe

--- start_video_stream2.sh
+++ start_video_stream2.sh
@@ -9,7 +9,9 @@ height=240
 echo "start_video_stream: starting video stream with server ip: $ip and port: $port"
 
 sudo modprobe bcm2835-v4l2
-rm outpipe
-mkfifo outpipe
+if [ -e /home/pi/robo-ops/pi-clients/outpipe ]; then
+rm /home/pi/robo-ops/pi-clients/outpipe
+fi
+mkfifo /home/pi/robo-ops/pi-clients/outpipe
 ffmpeg -loglevel fatal -threads 6 -s $width'x'$height -f video4linux2 -i /dev/video0 -f mpeg1video -b:v 400k -strict -1 -r $1 pipe:1 > /home/pi/robo-ops/pi-clients/outpipe &
 /home/pi/robo-ops/pi-clients/start_video.py $ip
