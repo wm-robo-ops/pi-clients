fd5a8dc9c895ebf8ab6e619b36e3f665ac427087
ajwells <ajwells@email.wm.edu>
2016-04-16 16:21:17 -0400

    change server address

--- restart.sh
+++ restart.sh
@@ -4,4 +4,4 @@ sudo pkill python
 sudo pkill ffmpeg
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 sudo bash /home/pi/robo-ops/pi-clients/repo_update.sh
-sudo python /home/pi/robo-ops/pi-clients/server-listener.py ec2-54-172-2-230.compute-1.amazonaws.com
+python /home/pi/robo-ops/pi-clients/server-listener.py ec2-54-85-173-111.compute-1.amazonaws.com &
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 bash /home/pi/robo-ops/pi-clients/repo_update.sh
-python /home/pi/robo-ops/pi-clients/server-listener.py ec2-54-172-2-230.compute-1.amazonaws.com &
+python /home/pi/robo-ops/pi-clients/server-listener.py ec2-54-85-173-111.compute-1.amazonaws.com &
