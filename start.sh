f790987765c2ee4371e822e6fc1b5abd81edec41
Aaron Wells <ajwells@email.wm.com>
2016-05-04 01:33:03 +0000

    restart now uses start. start now reverts fil eownership to pi:pi.

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ server=ec2-54-85-173-111.compute-1.amazonaws.com
 # Secondary
 server=ec2-54-83-155-188.compute-1.amazonaws.com
 
+sudo chown pi:pi *.sh *.py
+
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 bash /home/pi/robo-ops/pi-clients/repo_update.sh
 python /home/pi/robo-ops/pi-clients/server-listener.py $server &
