b5a50796dad0ac51ddd8d757e3b6151e96d9bb7b
Wouter Deconinck <wdconinc@gmail.com>
2016-05-05 21:29:53 +0000

    Change ownership on .git back to pi:pi since start.sh is called as root from rc.local.

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ server=ec2-54-85-173-111.compute-1.amazonaws.com
 server=ec2-54-83-155-188.compute-1.amazonaws.com
 
 sudo chown pi:pi *.sh *.py
+sudo chown -R pi:pi .git
 
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 bash /home/pi/robo-ops/pi-clients/repo_update.sh
