7448a5217f02c51c2f3be475efe7922c0c3009cc
Aaron Wells <ajwells@email.wm.com>
2016-05-09 16:03:15 +0000

    Nohup on python clients. Ignore nohup.out and other out files.

--- start.sh
+++ start.sh
@@ -10,4 +10,6 @@ sudo chown -R pi:pi .git
 
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 bash /home/pi/robo-ops/pi-clients/repo_update.sh
-python /home/pi/robo-ops/pi-clients/server-listener.py $server &
+
+nohup python /home/pi/robo-ops/pi-clients/server-listener.py $server > server_listener.out &
+disown
