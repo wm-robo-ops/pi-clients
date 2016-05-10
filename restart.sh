5db6e66a7fa3ca31ac6b1f95b7eade921678167b
Aaron Wells <ajwells@email.wm.com>
2016-05-10 10:42:20 +0000

    fix stopping gps and restart

--- restart.sh
+++ restart.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pkill python
+#sudo pkill python
+pkill -9 -f start_video.py
+pkill -9 -f start_gps.py
+pkill -9 -f dof_device.py
+pkill -9 -f server-listener.py
 sudo pkill ffmpeg
 sudo $HOME/robo-ops/pi-clients/start.sh
