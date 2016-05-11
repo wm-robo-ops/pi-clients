60c0836090bdc1e0be7d72fdde8ec73ed905eb22
Wouter Deconinck <wdconinc@gmail.com>
2016-05-11 21:07:33 +0000

    sudo when killing processes

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+	#!/usr/bin/env bash
 
 #sudo pkill python
-pkill -9 -f start_video.py
-pkill -9 -f start_gps.py
-pkill -9 -f dof_device.py
-pkill -9 -f server-listener.py
+sudo pkill -9 -f start_video.py
+sudo pkill -9 -f start_gps.py
+sudo pkill -9 -f dof_device.py
+sudo pkill -9 -f server-listener.py
 sudo pkill ffmpeg
 sudo /home/pi/robo-ops/pi-clients/start.sh
