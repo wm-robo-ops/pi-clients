81c35aa12b3a9c5a63f25dedc8093e7e6f833d77
Wouter Deconinck <wdconinc@gmail.com>
2016-05-12 18:51:32 +0000

    Script to restart all clients

---
+++ restart_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh pi@166.150.28.252 -p11000 /home/pi/robo-ops/pi-clients/restart.sh
+ssh pi@166.150.28.252 -p11001 /home/pi/robo-ops/pi-clients/restart.sh
+ssh pi@166.150.28.252 -p11002 /home/pi/robo-ops/pi-clients/restart.sh
+ssh pi@166.150.28.252 -p11003 /home/pi/robo-ops/pi-clients/restart.sh
+ssh pi@166.150.28.252 -p12000 /home/pi/robo-ops/pi-clients/restart.sh
