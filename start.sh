b1d93922334c4e4ed72a3414b24e2509b604ea1d
Wouter Deconinck <wdconinc@gmail.com>
2016-05-12 18:52:59 +0000

    New server

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ server=ec2-54-85-173-111.compute-1.amazonaws.com
 server=ec2-54-83-155-188.compute-1.amazonaws.com
 # Tertiary
 server=ec2-54-164-14-252.compute-1.amazonaws.com
+# Quartairy
+server=ec2-54-173-230-101.compute-1.amazonaws.com
 
 rm /home/pi/robo-ops/pi-clients/server_listener.log
 bash /home/pi/robo-ops/pi-clients/repo_update.sh
