8b50c1c048970a870b8c77d4665d346891f93bac
Aaron Wells <ajwells@email.wm.com>
2016-05-24 14:27:17 +0000

    Added 11005

--- restart_all.sh
+++ restart_all.sh
@@ -4,4 +4,5 @@ ssh pi@166.150.28.252 -p11000 /home/pi/robo-ops/pi-clients/restart.sh
 ssh pi@166.150.28.252 -p11001 /home/pi/robo-ops/pi-clients/restart.sh
 ssh pi@166.150.28.252 -p11002 /home/pi/robo-ops/pi-clients/restart.sh
 ssh pi@166.150.28.252 -p11003 /home/pi/robo-ops/pi-clients/restart.sh
+ssh pi@166.150.28.252 -p11005 /home/pi/robo-ops/pi-clients/restart.sh
 ssh pi@166.150.28.252 -p12000 /home/pi/robo-ops/pi-clients/restart.sh
